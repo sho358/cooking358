57808de10be140d7db5deae43e05a209f86e3822
sho358 <ishikawa.hokudai.2013@gmail.com>
2020-05-29 09:05:29 +0900

    ３章終わり

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,5 +23,5 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-  config.include Device::Test::IntegrationHelpers, type: :system
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end
