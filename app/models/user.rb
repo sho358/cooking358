2e00a8a0d1180064337876cd1213adfb51f25cdd
sho358 <ishikawa.hokudai.2013@gmail.com>
2020-06-02 20:03:20 +0900

    10章終わり

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,8 +45,11 @@ class User < ApplicationRecord
       update_attribute(:remember_digest, nil)
     end
 
-    def feed 
-      Dish.where("user_id = ?", id)
+    def feed
+      following_ids = "SELECT followed_id FROM relationships
+                       WHERE follower_id = :user_id"
+      Dish.where("user_id IN (#{following_ids})
+                       OR user_id = :user_id", user_id: id)
     end
 
     def follow(other_user)
